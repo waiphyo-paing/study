5e6e1b714d567b0f932b9341d8d40a40d1a4326d
waiphyo-paing <waiphyopaing.dev@gmail.com>
2024-05-25 21:43:15 +0700

    Initial commit after 8.2 Postgres read project with flag data

--- PostgreSql/8.2 Postgres READ/index.js
+++ PostgreSql/8.2 Postgres READ/index.js
@@ -1,8 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { configDotenv } from "dotenv";
+import pg from "pg";
+
+configDotenv();
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT
+});
+
+db.connect();
+
+let quiz = [];
+
+db.query("SELECT * FROM flags", (err, res) => {
+  if(err){
+    console.error("Error executing query", err.stack);
+  }else{
+    console.log(res.rows);
+    quiz = res.rows;
+  }
+});
 
 let totalCorrect = 0;
 
@@ -13,18 +38,18 @@ app.use(express.static("public"));
 let currentQuestion = {};
 
 // GET home page
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   totalCorrect = 0;
-  nextQuestion();
+  await nextQuestion();
   console.log(currentQuestion);
   res.render("index.ejs", { question: currentQuestion });
 });
 
 // POST a new post
 app.post("/submit", (req, res) => {
-  let answer = req.body.answer.trim();
+  let answer = req.body.answer;
   let isCorrect = false;
-  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+  if (currentQuestion.name.toLowerCase() === answer.toLowerCase()) {
     totalCorrect++;
     console.log(totalCorrect);
     isCorrect = true;
@@ -38,7 +63,7 @@ app.post("/submit", (req, res) => {
   });
 });
 
-function nextQuestion() {
+async function nextQuestion() {
   const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
   currentQuestion = randomCountry;
 }
