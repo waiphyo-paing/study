64c2e3007472b033a313a139c96ee63d5ac2cec6
waiphyo-paing <waiphyopaing.dev@gmail.com>
2024-05-29 21:44:13 +0700

    CRUD for task with database

--- PostgreSql/8.6 Permalist Project/index.js
+++ PostgreSql/8.6 Permalist Project/index.js
@@ -20,18 +20,9 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
-
 // Task Service
 class TaskService {
   constructor(){
-    // this.tasks = [
-    //   { id: 1, title: "Buy milk" },
-    //   { id: 2, title: "Finish homework" },
-    // ];
     this.tasks = [];
   }
 
@@ -45,12 +36,9 @@ class TaskService {
 
   // Get all tasks
   async getAllTasks(){
-    // return this.tasks;
-
     try{
-      const req = await db.query("SELECT * FROM items");
+      const req = await db.query("SELECT * FROM items ORDER BY id");
       this.tasks = req.rows;
-      console.log(this.tasks);
       return this.tasks;
     }catch(err){
       console.log("Error getting data.");
@@ -58,38 +46,50 @@ class TaskService {
     }
   }
 
-  // Add task
-  addTask(taskTitle){
-    const id = this.tasks.length + 1;
-    
+  // Search task with ID
+  async searchTask(id){
     try{
-      this.tasks.push(this.taskTemplate(id, taskTitle));
-      console.log(this.tasks);
+      const req = await db.query("SELECT * from items WHERE items.id=$1", [id]);
+      return req.rows;
+    }catch(err) {
+      console.log("Error getting task");
+      throw err;
+    }
+  }
+
+  // Add task
+  async addTask(taskTitle){
+    try{
+      await db.query("INSERT INTO items(title) VALUES ($1)", [taskTitle]);
+      await this.getAllTasks();
+
+      console.log("Added new data");
     }catch(err){
-      console.log("Having error at adding new Task: " + err);
+      console.log("Error adding data.");
+      throw err;
     }
   }
 
   // UPDATE task data
-  updateTask(taskId, taskTitle){
-    const task = this.tasks.find(t => t.id === taskId);
-
-    if(task){
-      task.title = taskTitle;
-
-      return this.tasks;
-    }else{
-      console.log("error");
+  async updateTask(taskId, taskTitle){
+    try{
+      await db.query("UPDATE items SET title=$1 WHERE id=$2", [taskTitle, taskId]);
+      await this.getAllTasks();
+    }catch(err){
+      console.log("Error updating task");
+      throw err;
     }
   }
 
   // DELETE task
-  deleteTask(taskId){
-    const task = this.tasks.find(t => t.id === taskId);
-    const taskIndex = parseInt(this.tasks.indexOf(task));
-
-    this.tasks.splice(taskIndex, 1);
-    console.log(this.tasks);
+  async deleteTask(taskId){
+    try{
+      await db.query("DELETE FROM items WHERE id=$1", [taskId]);
+      await this.getAllTasks();
+    }catch (err){
+      console.log("Error deleting task");
+      throw err;
+    }
   }
 }
 
@@ -98,48 +98,49 @@ const taskService = new TaskService();
 
 // Routing
 app.get("/", async (req, res) => {
-  const allTasks = await taskService.getAllTasks();
-
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: allTasks,
-  });
-});
-
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  try{
-    taskService.addTask(item);
-    res.redirect("/");
-  }catch(err){
-    console.log(err);
-    throw err;
+  try {
+    const allTasks = await taskService.getAllTasks();
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: allTasks,
+    });
+  } catch (err) {
+    console.log("Error retrieving tasks: ", err);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.post("/edit", (req, res) => {
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  try {
+    await taskService.addTask(item);
+    res.redirect("/");
+  } catch (err) {
+    console.log("Error adding task: ", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/edit", async (req, res) => {
   const taskId = parseInt(req.body['updatedItemId']);
   const taskTitle = req.body['updatedItemTitle'];
 
   try{
-    taskService.updateTask(taskId, taskTitle);
-    res.render("index.ejs", {
-      listTitle: "Today",
-      listItems: taskService.getAllTasks(),
-    });
+    await taskService.updateTask(taskId, taskTitle);
+    res.redirect('/');
   }catch(err){
-    console.log("Error");
-    throw err;
+    console.log("Error updating task: ", err);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.post("/delete", (req, res) => {
+app.post("/delete", async (req, res) => {
   try{
-    taskService.deleteTask(parseInt(req.body.deleteItemId));
-
+    await taskService.deleteTask(parseInt(req.body.deleteItemId));
     res.redirect('/');
   }catch(err){
-    console.log("Having error deleting: " + err);
+    console.log("Error deleting task: ", err);
+    res.status(500).send("Internal Server Error");
   }
 });
 
