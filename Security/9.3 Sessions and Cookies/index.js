c36787ade1f4ea910471b393f93e4940410e2939
waiphyo-paing <waiphyopaing.dev@gmail.com>
2024-05-31 00:16:03 +0700

    Initial commit after sessions and cookies for auth management

--- Security/9.3 Sessions and Cookies/index.js
+++ Security/9.3 Sessions and Cookies/index.js
@@ -2,6 +2,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import bcrypt from "bcrypt";
+import { configDotenv } from "dotenv";
+import session from "express-session";
+import passport from "passport";
+import { Strategy } from "passport-local";
+
+configDotenv();
 
 const app = express();
 const port = 3000;
@@ -10,12 +16,24 @@ const saltRounds = 10;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "TOPSECRET",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    maxAge: 1000 * 10
+  }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "123456",
-  port: 5432,
+  user: process.env.DATABASE_USER,
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  password: process.env.DATABASE_PASSWORD,
+  port: process.env.DATABASE_PORT
 });
 db.connect();
 
@@ -29,6 +47,14 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register.ejs");
+});
+
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets.ejs");
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.post("/register", async (req, res) => {
@@ -48,12 +74,16 @@ app.post("/register", async (req, res) => {
         if (err) {
           console.error("Error hashing password:", err);
         } else {
-          console.log("Hashed Password:", hash);
-          await db.query(
-            "INSERT INTO users (email, password) VALUES ($1, $2)",
+          const result = await db.query(
+            "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
             [email, hash]
           );
-          res.render("secrets.ejs");
+
+          const user = result.rows[0];
+          req.login(user, (err) => {
+            if (err) { console.log(err); }
+            res.redirect('/secrets');
+          })
         }
       });
     }
@@ -62,34 +92,45 @@ app.post("/register", async (req, res) => {
   }
 });
 
-app.post("/login", async (req, res) => {
-  const email = req.body.username;
-  const loginPassword = req.body.password;
+app.post("/login", passport.authenticate('local', {
+  successRedirect: "/secrets",
+  failureRedirect: "/login"
+}));
 
+passport.use(new Strategy(async function verify(username, password, cb) {
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
-      email,
+      username
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
       const storedHashedPassword = user.password;
-      bcrypt.compare(loginPassword, storedHashedPassword, (err, result) => {
+      bcrypt.compare(password, storedHashedPassword, (err, result) => {
         if (err) {
-          console.error("Error comparing passwords:", err);
+          return cb(err);
         } else {
           if (result) {
-            res.render("secrets.ejs");
+            return cb(null, user);
           } else {
-            res.send("Incorrect Password");
+            return cb(null, false, { message: "Incorrect username or password." });
           }
         }
       });
     } else {
-      res.send("User not found");
+      return cb(null, false, { message: "User not found!" });
     }
   } catch (err) {
     console.log(err);
+    return cb(err);
   }
+}));
+
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
 });
 
 app.listen(port, () => {
